55deaf997b60f1a9b97edc58c5a684f48a301afd
Anupam Krishna <anupamkris13262@gmail.com>
2024-02-16 11:18:43 +0530

    upd

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.startsWith("ion-icon"),
+        },
+      },
+    }),
+    AutoImport({
+      dirs: [],
+      imports: ["vue"],
+    }),
+    Components({
+      dirs: ["src/components"],
+    }),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+});
